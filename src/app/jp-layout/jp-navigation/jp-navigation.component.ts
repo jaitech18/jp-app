c298ef5882bb2c53798d965121e311227673e553
jaitech18 <jaitech18@gmail.com>
2022-06-11 17:35:17 +0530

    publish changes
    publish

---
+++ src/app/jp-layout/jp-navigation/jp-navigation.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-jp-navigation',
+  templateUrl: './jp-navigation.component.html',
+  styleUrls: ['./jp-navigation.component.css']
+})
+export class JpNavigationComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
